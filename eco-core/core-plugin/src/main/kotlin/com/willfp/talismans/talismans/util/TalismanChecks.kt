af21a820982bdde716ecb1854a22a2234e1ce447
MMN <64864304+MMN-2024@users.noreply.github.com>
2025-09-22 21:25:38 +0200

    Fix TalismanBag cache clearing for immediate effect updates

--- eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/talismans/util/TalismanChecks.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/talismans/util/TalismanChecks.kt
@@ -6,6 +6,7 @@ import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.config.updating.ConfigUpdater
 import com.willfp.eco.core.fast.fast
 import com.willfp.libreforge.ItemProvidedHolder
+import com.willfp.talismans.bag.TalismanBag
 import com.willfp.talismans.TalismansPlugin.Companion.instance
 import com.willfp.talismans.talismans.Talisman
 import com.willfp.talismans.talismans.Talismans.getByID
@@ -185,6 +186,7 @@ object TalismanChecks {
     fun clearCache(player: Player) {
         CACHED_TALISMAN_ITEMS.invalidate(player)
         CACHED_TALISMANS.invalidate(player)
+        TalismanBag.clearCache(player)
     }
 
     /**
