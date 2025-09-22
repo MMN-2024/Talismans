f81a0ff175800a71410e0a11cfbd156bc92e56e7
MMN <64864304+MMN-2024@users.noreply.github.com>
2025-09-22 22:06:41 +0200

    Add debug logging to identify which events are firing

--- eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/talismans/util/TalismanInventoryListener.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/talismans/util/TalismanInventoryListener.kt
@@ -4,6 +4,7 @@ import com.willfp.libreforge.updateEffects
 import com.willfp.libreforge.toDispatcher
 import com.willfp.talismans.TalismansPlugin
 import org.bukkit.Bukkit
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -23,48 +24,59 @@ class TalismanInventoryListener(private val plugin: TalismansPlugin) : Listener 
         // Clear talisman cache and update effects immediately
         TalismanChecks.clearCache(player)
         player.toDispatcher().updateEffects()
+        
+        // Debug logging
+        plugin.logger.info("Refreshed effects for player: ${player.name}")
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onInventoryClick(event: InventoryClickEvent) {
         val player = event.whoClicked as? Player ?: return
+        plugin.logger.info("InventoryClickEvent triggered for player: ${player.name}")
         refreshPlayerEffects(player)
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onInventoryDrag(event: InventoryDragEvent) {
         val player = event.whoClicked as? Player ?: return
+        plugin.logger.info("InventoryDragEvent triggered for player: ${player.name}")
         refreshPlayerEffects(player)
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onPlayerItemHeld(event: PlayerItemHeldEvent) {
+        plugin.logger.info("PlayerItemHeldEvent triggered for player: ${event.player.name}")
         refreshPlayerEffects(event.player)
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onPlayerDropItem(event: PlayerDropItemEvent) {
+        plugin.logger.info("PlayerDropItemEvent triggered for player: ${event.player.name} - Item: ${event.itemDrop.itemStack.type}")
         refreshPlayerEffects(event.player)
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onPlayerPickupItem(event: PlayerPickupItemEvent) {
+        plugin.logger.info("PlayerPickupItemEvent triggered for player: ${event.player.name} - Item: ${event.item.itemStack.type}")
         refreshPlayerEffects(event.player)
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onEntityPickupItem(event: EntityPickupItemEvent) {
         val player = event.entity as? Player ?: return
+        plugin.logger.info("EntityPickupItemEvent triggered for player: ${player.name} - Item: ${event.item.itemStack.type}")
         refreshPlayerEffects(player)
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onPlayerSwapHandItems(event: PlayerSwapHandItemsEvent) {
+        plugin.logger.info("PlayerSwapHandItemsEvent triggered for player: ${event.player.name}")
         refreshPlayerEffects(event.player)
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onPlayerItemBreak(event: PlayerItemBreakEvent) {
+        plugin.logger.info("PlayerItemBreakEvent triggered for player: ${event.player.name} - Item: ${event.getBrokenItem().type}")
         refreshPlayerEffects(event.player)
     }
 }
