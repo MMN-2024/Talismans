8b122cad32d0e936dfd6dc513203eb70980b1d86
MMN <64864304+MMN-2024@users.noreply.github.com>
2025-09-22 22:01:57 +0200

    Add InventoryDragEvent listener to fix effect persistence

--- eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/talismans/util/TalismanInventoryListener.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/talismans/util/TalismanInventoryListener.kt
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryDragEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerItemHeldEvent
 import org.bukkit.event.player.PlayerPickupItemEvent
@@ -24,6 +25,12 @@ class TalismanInventoryListener(private val plugin: TalismansPlugin) : Listener 
     
     @EventHandler(priority = EventPriority.MONITOR)
     fun onInventoryClick(event: InventoryClickEvent) {
+        val player = event.whoClicked as? Player ?: return
+        refreshPlayerEffects(player)
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onInventoryDrag(event: InventoryDragEvent) {
         val player = event.whoClicked as? Player ?: return
         refreshPlayerEffects(player)
     }
